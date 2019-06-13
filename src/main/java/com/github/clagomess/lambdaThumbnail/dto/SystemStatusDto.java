389548d5713ade16a0630fd6598046c77b1c7b8f
CLAUDIO GOMES <claudio.silva@fnde.gov.br>
2019-06-13 17:38:57 -0300

    release 1.0.2

--- src/main/java/com/github/clagomess/lambdaThumbnail/dto/SystemStatusDto.java
+++ src/main/java/com/github/clagomess/lambdaThumbnail/dto/SystemStatusDto.java
@@ -1,6 +1,5 @@
 package com.github.clagomess.lambdaThumbnail.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.sql.Timestamp;
@@ -8,8 +7,5 @@ import java.util.Calendar;
 
 @Data
 public class SystemStatusDto {
-    private String deployTag = getClass().getPackage().getImplementationVersion();
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss z", timezone = "America/Sao_Paulo")
     private Timestamp aplicacaoData = new Timestamp(Calendar.getInstance().getTime().getTime());
 }
